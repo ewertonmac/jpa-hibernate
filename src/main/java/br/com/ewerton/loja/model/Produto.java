faa4124f3d541b3f1cc1a7da47cc7a009b21edce
Ewerton Maciel <ewertoncz@gmail.com>
2022-07-17 18:40:11 -0300

    configurando hibernate e definindo entidade

---
+++ src/main/java/br/com/ewerton/loja/model/Produto.java
@@ -0,0 +1,22 @@
+package br.com.ewerton.loja.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+
+}
